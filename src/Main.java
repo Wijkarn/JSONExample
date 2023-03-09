aa8c3257b098ff975b39e6cd48e0721df10829d5
Wijkarn <mathias.wijk@hotmail.com>
2023-03-09 10:27:59 +0100

    Skappat ett JSON Object och skrivit och hämtat data vi den.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import org.json.simple.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
         System.out.println("Deez nutz");
+
+        // Skapa ett JSON Object
+        JSONObject jsonOb = new JSONObject();
+
+        //Spara värden in JSON Object
+        jsonOb.put("namn","Wijk");
+        jsonOb.put("age","22");
+
+        //Skriva ut värden
+        System.out.println("Mitt namn är: " + jsonOb.get("namn"));
+        System.out.println("Jag är " + jsonOb.get("age") + " år gammal.");
+
 
     }
 }
